732812591000a6e95f3ba37e94d0a3f3a5bd2248
Adam A.G. Shamblin <adam.shamblin@gmail.com>
2014-05-04 22:16:06 -0600

    Update README, sketch solver.

--- solver.py
+++ solver.py
@@ -6,13 +6,27 @@ class Solver (object):
     def __init__(self):
         self.game = Game()
 
+    def play(self):
+
+        board = self.game.current_state
+
+        while(board is not None):
+            board = self.best_move(board)
+
+    def best_move(self, board):
+        '''Process.
+
+        1. Establish first board.
+        2. Play each possible move.
+        3. Evaluate the value of each possible move.
+            . Score of move
+            . Empty spaces on the board
+        4. Commit to the "best" move.
+        5. Repeat
+        '''
+        pass
+
 
 if __name__ == '__main__':
     solver = Solver()
-    for i in range(10):
-        solver.game.move('UP')
-        solver.game.move('DOWN')
-        solver.game.move('LEFT')
-        solver.game.move('RIGHT')
-
-    print solver.game.score
+    solver.play()
