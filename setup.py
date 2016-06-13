f8d427f1e4f09c25492429961a16a75536792a53
Adam A.G. Shamblin <adam.shamblin@hpe.com>
2016-06-13 17:17:31 -0600

    Rearrange files for packaging.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='py2048',
+      version='0.1',
+      description='Modules and objects for solving the game 2048',
+      author='Adam A.G. Shamblin',
+      author_email='adam.shamblin@tutanota.com',
+      license='MIT')
